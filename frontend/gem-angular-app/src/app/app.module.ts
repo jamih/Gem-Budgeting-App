8439bab7f7d9a26a306bc35476a0525f7808a80b
Jami <jamih@bu.edu>
2024-05-13 12:26:33 -0400

    added front end set up

---
+++ frontend/gem-angular-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from '../header/header.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
