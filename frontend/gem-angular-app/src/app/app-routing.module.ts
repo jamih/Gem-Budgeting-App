03e07b205c0d8087f340fc2c298050271351b36c
crissr11 <cris.ruiz07@protonmail.com>
2024-05-14 11:10:35 -0700

    created base expenses page

--- frontend/gem-angular-app/src/app/app-routing.module.ts
+++ frontend/gem-angular-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
+import { ExpensesComponent } from './expenses/expenses.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'expenses', component: ExpensesComponent },
 ];
 
 @NgModule({
