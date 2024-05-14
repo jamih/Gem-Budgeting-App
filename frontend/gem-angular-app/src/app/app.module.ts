98d6890c414a352243d8a80c6e7fd82f16000d3c
Jami <jamih@bu.edu>
2024-05-14 12:50:19 -0400

    fixed logo positioning, added home components

--- frontend/gem-angular-app/src/app/app.module.ts
+++ frontend/gem-angular-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from '../header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
