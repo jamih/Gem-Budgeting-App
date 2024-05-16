ee49579c847cd8cd5c3822c4f8d7d3aa675e03b3
Jami <jamih@bu.edu>
2024-05-16 17:52:29 -0500

    added components for registration page

--- frontend/gem-angular-app/src/app/app-routing.module.ts
+++ frontend/gem-angular-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: '', redirectTo: '/registration', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'registration', component: RegistrationComponent}
 ];
 
 @NgModule({
